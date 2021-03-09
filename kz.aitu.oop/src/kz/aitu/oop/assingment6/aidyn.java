1daadbee695fd7396326e584a9874bccd0c6ac46
naymanCode <80192503+naymanCode@users.noreply.github.com>
2021-03-09 21:54:59 +0600

    Update aidyn.java

--- kz.aitu.oop/src/kz/aitu/oop/assingment6/aidyn.java
+++ kz.aitu.oop/src/kz/aitu/oop/assingment6/aidyn.java
@@ -4,19 +4,14 @@ public class aidyn {
 
 	public static void main(String[] args) {
 		Logistics first = createLogistics("Truck");
-		Logistics second = createLogistics("Drone");
 		Transport j1 = first.createTransport();
-		Transport j2 = second.createTransport();
 		first.planDelivery();
 		j1.deliver();
-		second.planDelivery();
-		j2.deliver();
 	}
 	public static Logistics createLogistics(String transport){
 		switch (transport) {
 		case "Ship" : return new SeaLogistics();
 		case "Truck" : return new RoadLogistics();
-		case "Drone" : return new AirLogistics();
 		default: return null;//
 		}
 		
